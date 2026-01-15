15655057424efb15413762eb8e7d47342ec80f2d
waiting <1491361526@qq.com>
2026-01-15 12:33:47 +0800

    add ->V1.8dio分装

--- lib/global.dart
+++ lib/global.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'common/index.dart';
+import 'common/services/wp_http.dart';
 
 class Global {
   static Future<void> init() async {
@@ -11,10 +12,11 @@ class Global {
     // 工具类
     await Storage().init();
 
-    // 初始化队列
-    await Future.wait([
-      // 配置服务
-      Get.putAsync<ConfigService>(() async => await ConfigService().init()),
-    ]).whenComplete(() {});
+    // 初始化服务
+    Get.put<ConfigService>(ConfigService());
+    Get.put<WPHttpService>(WPHttpService());
+
+    // 初始化配置
+    await ConfigService.to.init();
   }
 }
