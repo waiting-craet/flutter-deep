9386ef3b1c0e0c2d0b01e7745189a0de272395cb
waiting <1491361526@qq.com>
2026-01-15 18:01:31 +0800

    add ->样式配置V1.10

--- lib/common/style/index.dart
+++ lib/common/style/index.dart
@@ -2,3 +2,8 @@ library style;
 
 export 'colors.dart';
 export 'theme.dart';
+export 'size.dart';
+export 'radius.dart';
+export 'padding.dart';
+export 'elevation.dart';
+export 'Border .dart';
--- lib/common/style/theme.dart
+++ lib/common/style/theme.dart
@@ -21,13 +21,37 @@ class AppTheme {
 
   /// 亮色主题
   static ThemeData get light {
-    ColorScheme scheme = MaterialTheme.lightScheme();
+    ColorScheme scheme = MaterialTheme.lightScheme().copyWith(
+      primary: primary,
+      onPrimary: Colors.white,
+      secondary: secondary,
+      onSecondary: Colors.white,
+      surface: Colors.white,
+      onSurface: const Color(0xFF333333),
+      error: error,
+      onError: Colors.white,
+      //   tertiary: const Color(0xFFF4F6F9),
+      //   outline: const Color(0xFFF4F6F9),
+      //   shadow: const Color(0xFF5A6CEA).withOpacity(0.08),
+    );
     return _getTheme(scheme);
   }
 
   /// 暗色主题
   static ThemeData get dark {
-    ColorScheme scheme = MaterialTheme.darkScheme();
+    ColorScheme scheme = MaterialTheme.darkScheme().copyWith(
+      primary: primary,
+      // onPrimary: Colors.white,
+      secondary: secondary,
+      // onSecondary: Colors.white,
+      // surface: const Color(0xFF252525),
+      // onSurface: Colors.white,
+      error: error,
+      onError: Colors.white,
+      //   tertiary: const Color(0xFF141414),
+      //   outline: const Color(0xFF252525),
+      //   shadow: const Color(0xFF777777).withOpacity(0.08),
+    );
     return _getTheme(scheme);
   }
 
