0fed08e3430ef4c3d3574fe5f9c284ff1c3f93e7
waiting <1491361526@qq.com>
2026-01-16 16:32:56 +0800

    add ->Screenutil配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:ducafe_ui_core/ducafe_ui_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_deep/common/index.dart';
 import 'package:get/get.dart';
@@ -17,33 +18,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AdaptiveTheme(
-        // 样式
-        light: AppTheme.light, // 亮色主题
-        dark: AppTheme.dark, // 暗色主题
-        initial: ConfigService.to.themeMode, // 初始主题
-        debugShowFloatingThemeButton: true, // 显示主题按钮
-        //构建
-        builder: (theme, darkTheme) => GetMaterialApp(
-              title: 'Flutter Demo',
-              // 主题
-              // 主题
-              theme: theme,
-              darkTheme: darkTheme,
+      // 样式
+      light: AppTheme.light, // 亮色主题
+      dark: AppTheme.dark, // 暗色主题
+      initial: ConfigService.to.themeMode, // 初始主题
+      debugShowFloatingThemeButton: true, // 显示主题按钮
+      //构建
+      builder: (theme, darkTheme) => ScreenUtilInit(
+        designSize: const Size(414, 896), // 设计稿中设备的尺寸(单位随意,建议dp,但在使用过程中必须保持一致)
+        splitScreenMode: false, // 支持分屏尺寸
+        minTextAdapt: false, // 是否根据宽度/高度中的最小值适配文字
+        builder: (context, child) => GetMaterialApp(
+          title: 'Flutter Demo',
+          // 主题
+          theme: theme,
+          darkTheme: darkTheme,
 
-              // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          // home: const MyHomePage(title: 'Flutter Demo Home Page'),
 
-              // 路由
-              //initialRoute: RouteNames.systemSplash,
-              initialRoute: RouteNames.stylesStylesIndex,
-              getPages: RoutePages.list,
-              navigatorObservers: [RoutePages.observer],
-              // 多语言
-              translations: Translation(), // 词典
-              localizationsDelegates: Translation.localizationsDelegates, // 代理
-              supportedLocales: Translation.supportedLocales, // 支持的语言种类
-              locale: ConfigService.to.locale, // 当前语言种类
-              fallbackLocale: Translation.fallbackLocale, // 默认语言种类
-            ));
+          // 路由
+          //initialRoute: RouteNames.systemSplash,
+          initialRoute: RouteNames.stylesStylesIndex,
+          getPages: RoutePages.list,
+          navigatorObservers: [RoutePages.observer],
+          // 多语言
+          translations: Translation(), // 词典
+          localizationsDelegates: Translation.localizationsDelegates, // 代理
+          supportedLocales: Translation.supportedLocales, // 支持的语言种类
+          locale: ConfigService.to.locale, // 当前语言种类
+          fallbackLocale: Translation.fallbackLocale, // 默认语言种类
+
+          // builder
+          builder: (context, widget) {
+            // 不随系统字体缩放比例
+            return MediaQuery(
+              data: MediaQuery.of(context)
+                  .copyWith(textScaler: const TextScaler.linear(1.0)),
+              child: widget!,
+            );
+          },
+
+          // 隐藏 debug 标志
+          debugShowCheckedModeBanner: false,
+        ),
+      ),
+    );
   }
 }
 
